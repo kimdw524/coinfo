8ca17258b453e92fb8f568468b5d8d7082cff680
kimdw524 <kimdw524@naver.com>
2025-04-10 04:52:47 +0900

    fix(ui): globalStyle color hsl -> rgb 변경

--- packages/ui/src/styles/globalStyle.css.ts
+++ packages/ui/src/styles/globalStyle.css.ts
@@ -3,17 +3,15 @@ import { globalFontFace, globalStyle } from '@vanilla-extract/css';
 import Pretendard from '../assets/fonts/PretendardVariable.woff2';
 import { theme } from '#themes';
 
-globalFontFace('Pretendard', [
-  {
-    src: `url(${Pretendard}) format("woff2")`,
-  },
-]);
+globalFontFace('Pretendard', {
+  src: `url(${Pretendard}) format("woff2")`,
+});
 
 globalStyle('*', {
   fontFamily: 'Pretendard',
 });
 
 globalStyle('body', {
-  backgroundColor: `hsl(${theme.color.background})`,
-  color: `hsl(${theme.color.foreground})`,
+  backgroundColor: `rgb(${theme.color.background})`,
+  color: `rgb(${theme.color.foreground})`,
 });
