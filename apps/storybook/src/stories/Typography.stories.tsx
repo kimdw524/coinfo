e5e4bcdb6816abb9495f4ef3ceb98e95b0b49fe3
kimdw524 <kimdw524@naver.com>
2025-04-10 06:18:30 +0900

    feat(storybook): Typography story 추가

---
+++ apps/storybook/src/stories/Typography.stories.tsx
@@ -0,0 +1,48 @@
+import { Typography, typographyCss } from '@repo/ui';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'UI/Typography',
+  component: Typography,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    children: {
+      control: 'text',
+    },
+    color: {
+      control: 'color',
+    },
+    as: {
+      control: 'select',
+      options: ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'],
+    },
+    size: {
+      control: 'select',
+      options: Object.keys(typographyCss.typography.classNames.variants.size),
+    },
+    weight: {
+      control: 'select',
+      options: Object.keys(typographyCss.typography.classNames.variants.weight),
+    },
+    ellipsis: {
+      control: 'boolean',
+    },
+    style: {
+      control: false,
+    },
+  },
+} satisfies Meta<typeof Typography>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const TypographyStory: Story = {
+  args: {
+    children: 'Typography',
+  },
+  name: 'Typography',
+};
