2f34b05a8f5ca0ca2c7d494e66149dc5dd9b4561
kimdw524 <kimdw524@naver.com>
2025-04-11 04:33:07 +0900

    fix(ui): global font 제거

--- packages/ui/src/styles/globalStyle.css.ts
+++ packages/ui/src/styles/globalStyle.css.ts
@@ -1,15 +1,6 @@
-import { globalFontFace, globalStyle } from '@vanilla-extract/css';
+import { globalStyle } from '@vanilla-extract/css';
 
-import Pretendard from '../assets/fonts/PretendardVariable.woff2';
 import { theme } from '#themes';
-
-globalFontFace('Pretendard', {
-  src: `url(${Pretendard}) format("woff2")`,
-});
-
-globalStyle('*', {
-  fontFamily: 'Pretendard',
-});
 
 globalStyle('body', {
   backgroundColor: `rgb(${theme.color.background})`,
