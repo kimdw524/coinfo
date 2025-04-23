ce49c1e95dff2208fa351bd6de64b1e6072c45c2
kimdw524 <kimdw524@naver.com>
2025-04-24 05:19:03 +0900

    design(ui): globalStyle 수정

--- packages/ui/src/styles/globalStyle.css.ts
+++ packages/ui/src/styles/globalStyle.css.ts
@@ -3,6 +3,18 @@ import { globalStyle } from '@vanilla-extract/css';
 import { theme } from '#themes';
 
 globalStyle('body', {
+  margin: '0',
+
   backgroundColor: `rgb(${theme.color.background})`,
+
   color: `rgb(${theme.color.foreground})`,
+});
+
+globalStyle('*', {
+  boxSizing: 'border-box',
+
+  fontFamily: 'Pretendard',
+
+  overscrollBehavior: 'none',
+  WebkitTapHighlightColor: 'transparent',
 });
