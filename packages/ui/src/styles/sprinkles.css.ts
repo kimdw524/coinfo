d096a9e3799ab1fedfcf818ddc2f8c28ef61dee2
kimdw524 <kimdw524@naver.com>
2025-04-29 03:26:17 +0900

    feat(ui): sprinkles 정의

---
+++ packages/ui/src/styles/sprinkles.css.ts
@@ -0,0 +1,66 @@
+import { createSprinkles, defineProperties } from '@vanilla-extract/sprinkles';
+
+import { theme } from '#themes';
+import { color } from '#tokens';
+
+type ColorName = keyof typeof color;
+type ColorScale<C extends ColorName> = keyof (typeof color)[C];
+type Color = Record<`${ColorName}-${ColorScale<ColorName>}`, string>;
+type SemanticColor = Record<keyof typeof theme.color, string>;
+
+const colors = Object.assign(
+  {},
+  ...[
+    ...Object.entries(color).reduce(
+      (prev, [name, scales]) => [
+        ...prev,
+        ...Object.entries(scales).map(([scale, value]) => ({ [`${name}-${scale}`]: `rgb(${value})` }) as Color),
+      ],
+      [] as Color[],
+    ),
+    ...Object.entries(theme.color).map(([name, value]) => ({ [name]: `rgb(${value})` }) as SemanticColor),
+  ],
+) as Record<keyof Color | keyof SemanticColor, string>;
+
+export const colorProperties = defineProperties({
+  properties: {
+    color: colors,
+    backgroundColor: colors,
+  },
+});
+
+const space = {
+  none: 0,
+  sm: '0.25rem',
+  md: '0.5rem',
+  lg: '0.75rem',
+  xl: '1rem',
+};
+
+export const boxProperties = defineProperties({
+  properties: {
+    flexDirection: ['row', 'column'],
+    justifyContent: ['stretch', 'flex-start', 'center', 'flex-end', 'space-around', 'space-between'],
+    alignItems: ['stretch', 'flex-start', 'center', 'flex-end'],
+    paddingTop: space,
+    paddingBottom: space,
+    paddingLeft: space,
+    paddingRight: space,
+    marginTop: space,
+    marginBottom: space,
+    marginLeft: space,
+    marginRight: space,
+  },
+  shorthands: {
+    padding: ['paddingTop', 'paddingBottom', 'paddingLeft', 'paddingRight'],
+    paddingX: ['paddingLeft', 'paddingRight'],
+    paddingY: ['paddingTop', 'paddingBottom'],
+    margin: ['marginTop', 'marginBottom', 'marginLeft', 'marginRight'],
+    marginX: ['marginLeft', 'marginRight'],
+    marginY: ['marginTop', 'marginBottom'],
+  },
+});
+
+export const sprinkles = createSprinkles(boxProperties, colorProperties);
+
+export type SprinklesProps = Parameters<typeof sprinkles>[0];
