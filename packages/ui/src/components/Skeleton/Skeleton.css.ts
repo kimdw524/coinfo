40d3794c584118d355574f61e839b6e52b319d7c
kimdw524 <kimdw524@naver.com>
2025-04-30 02:36:12 +0900

    refactor(ui): Skeleton maxWidth 추가

--- packages/ui/src/components/Skeleton/Skeleton.css.ts
+++ packages/ui/src/components/Skeleton/Skeleton.css.ts
@@ -14,6 +14,7 @@ const shimmer = keyframes({
 export const skeleton = style({
   display: 'inline-block',
 
+  maxWidth: '100%',
   borderRadius: theme.borderRadius,
 
   background: `linear-gradient(90deg, rgb(${theme.color.accent}) 30%, rgba(${theme.color.accent}, 0.5) 65%, rgb(${theme.color.accent}) 100%)`,
