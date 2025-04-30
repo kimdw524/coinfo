942a610f1c49092e9335262811ba6037ab7b988f
kimdw524 <kimdw524@naver.com>
2025-05-01 04:52:02 +0900

    feat(web): 미국 시장 지표 API 구현

---
+++ apps/web/src/app/api/index/route.ts
@@ -0,0 +1,37 @@
+const getItem = (source: string, name: string) => {
+  const data = source.split(name)[1]?.split('[')[1]?.split(']')[0]?.split(',');
+
+  if (!data) {
+    throw new Error();
+  }
+
+  return {
+    value: Math.round(Number(data[0]) * 100) / 100,
+    change: Math.round(Number(data[1]) * 100) / 100,
+  };
+};
+
+export const revalidate = 9;
+
+export async function GET() {
+  try {
+    const data = await fetch('https://www.google.com/finance/markets/indexes/americas');
+    const res = await data.text();
+
+    const source = res.split('window.IJ_values =')[1];
+
+    if (!source) {
+      throw new Error();
+    }
+
+    return Response.json({
+      USDKRW: getItem(source, 'USD / KRW'),
+      INDEXDJX: getItem(source, '[".DJI","INDEXDJX"]'),
+      INDEXSP: getItem(source, '[".INX","INDEXSP"]'),
+      INDEXNASDAQ: getItem(source, '["NDX","INDEXNASDAQ"]'),
+    });
+  } catch (error) {
+    console.error(error);
+    return Response.error();
+  }
+}
